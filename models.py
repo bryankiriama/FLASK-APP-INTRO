51413fcdf5725b6490e0bdcfd5935eaf59cac8af
bryankiriama <bryankiriama@gmail.com>
2025-12-19 23:28:34 +0300

    used delete  method on models

--- models.py
+++ models.py
@@ -16,8 +16,8 @@ class User(db.Model):
         __tablename__ = 'posts'
         id = db.Column(db.Integer, primary_key=True)
         title = db.Column(db.String(200), nullable=False)
-        content = db.Column(db.Text, nullable=False)
-        user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+        # content = db.Column(db.Text, nullable=False)
+        # user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
         # user = db.relationship('User', backref=db.backref('posts', lazy=True))
 
